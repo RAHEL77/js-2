c228fe1d7573c472d56348bc01ad86aed28a754d
rachel <baruh.rahel@gmail.com>
2021-02-20 13:03:32 +0200

    1

---
+++ 1.1.js
@@ -0,0 +1,10 @@
+function bool(a) {
+    if (a == false)
+        console.log("No")
+    else
+        console.log("Yes")
+
+    //    console.log(a == t  rue ? "Yes" : "No")
+}
+const x = new Boolean(1);
+bool(x);
